cbad2585657715851bc0bd2f7cc02d090d3ada6f
Dhruv bansal <cybosecdhruv@gmail.com>
2026-06-06 16:39:44 +0530

    database online

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,23 @@
 const { Pool } = require("pg");
 
+//local//
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "digital_marketing",
+//   password: "Cybo#2006",
+//   port: 5432,
+// });
+
+//online//
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "digital_marketing",
-  password: "Cybo#2006",
-  port: 5432,
+  connectionString:
+    process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
+
+
 
 module.exports = pool;
